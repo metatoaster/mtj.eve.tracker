cf7f6cf9520bdeff7e701e75767a8f38b877451b
Tommy Yu <y@metatoaster.com>
2012-12-09 15:42:35 +1300

    Organizing the location of attributes.

--- mtj/eve/tracker/pos.py
+++ mtj/eve/tracker/pos.py
@@ -18,6 +18,19 @@ class Reinforcement(Event):
     """
 
 
+def lookupTower(itemid):
+    # XXX placeholder
+    items = {
+        12235: {
+            'name': 'Amarr Control Tower',
+            'fuelname': 'Amarr Fuel Block',
+            'fuelrate': 40,
+        }
+    }
+
+    return items.get(itemid, {})
+
+
 class TowerFactory(InstallationFactory):
     """
     Factory to build a POS.
@@ -26,11 +39,15 @@ class TowerFactory(InstallationFactory):
     def __init__(self):
         pass
     
-    def __call__(self, system, planet, moon):
-        tower = Tower(system, planet, moon)
-        tower.system = system
-        tower.planet = planet
-        tower.moon = moon
+    def __call__(self, system, planet, moon, state, itemid):
+
+        info = lookupTower(itemid)
+
+        tower = Tower(system, planet, moon, state)
+        # register this into the database.
+
+        tower.itemid = itemid
+
         return tower
 
 
@@ -39,34 +56,127 @@ class Tower(Installation):
     A Player Owned Structure (POS).
     """
 
-    def __init__(self, system, planet, moon):
-        pass
-
-
-class TowerFuelBay(TimedBuffer):
-
-    def __init__(self, value, size):
+    def __init__(self, system, planet, moon, state):
         """
+        This should only be called once.
         """
 
-        # delta is variable, as determined by size (immutable) and sov
-        # status.
-        delta = 40
+        self.system = system
+        self.planet = planet
+        self.moon = moon
+        # one of {unanchored, anchoring, anchored, online, reinforced}
+        self.state = state
 
-        # if for whatever reason a delta need to be forced (due to API
-        # mismatch, tower size mismatch, sov status mismatch) this can
-        # be overridden using this.  Use an event.
-        force_delta = None
+        # Can be updated, but generally derived from evedb
+        self.empire = None  # derived from system/evedb
+        self.itemid = None  # tower typeid
+        self.itemname = None  # derived, 'Amarr Control Tower'
+        self.fuelid = None  # derived, 4548 (?)
+        self.fuelname = None  # derived, 'Amarr Fuel Block'
+        self.fuelrate = None  # derived, 'Amarr Fuel Block'
 
-        super(TowerFuelBay, self).__init__(
+        # demo
+        self.empire = False
+        self.itemname = 'Amarr Control Tower'
+        self.fuelname = 'Amarr Fuel Block'
+        self.base_fuel_rate = 40
+
+        # delta is determined by the tower (or this parent), but still
+        # have to be provided as a standard parameter (rather than a
+        # calculated one) to satisfy parent class invariants.
+        # if for whatever reason a delta mismatch occurs due to API
+        # mismatch, tower size mismatch, sov status mismatch there 
+        # should be a way to override the delta until the condition
+        # triggering this is corrected.
+
+        fuel_rate = self.fuel_rate
+
+        # The calculated moving parts
+        self.resources = {
+            self.fuelname: TowerResourceBuffer(self, delta, None, 0),
+            'Strontium': TowerResourceBuffer(self, 1, None, 0, freeze=True),
+        }
+
+        if self.empire:
+            self.resources.update({
+                'charter': TowerResourceBuffer(self, delta, None, 0),
+            })
+
+    @property
+    def fuel_rate(self):
+        return self.base_fuel_rate
+
+    def updateResources(self):
+        raise NotImplementedError()
+
+        # loop through all resources
+        # while doing that, check that the states are consistent with
+        # what the tower is representing
+        # commit the states if they differ.
+
+
+class TowerResourceBuffer(TimedBuffer):
+    """
+    The base tower bay.
+    """
+
+    def __init__(self, tower=None, delta=None, timestamp=None, value=0, 
+            *a, **kw):
+
+        self.tower = tower
+
+        super(TowerResourceBuffer, self).__init__(
             delta=delta,
+            # one hour
             period=3600,
-            timestamp=None,
-            delta_min=1,  # full consumption
-            delta_factor=-1,  # depletes
+            timestamp=timestamp,
+            # full consumption
+            delta_min=1,
+            # depletes
+            delta_factor=-1,
+            # always running, determined by below
             freeze=False,
-            extra_freeze_condition=None,
-            full=sys.maxint,  # No real upper limit, it depletes anyway
+            extra_freeze_conditions=None,
+            # No real upper limit due to the possibility to stuff more 
+            # items into fuel bay because :ccp:
+            full=sys.maxint,
             value=value,
+            # Empty level always 0.  Fortunately.
             empty=0,
         )
+
+
+class TowerStrontiumBuffer(TowerResourceBuffer):
+
+    def __init__(self, tower=None, delta=None, timestamp=None, value=0, 
+            *a, **kw):
+
+        self.tower = tower
+
+        super(TowerResourceBuffer, self).__init__(
+            # Always unit size 1
+            delta=delta,
+            # one hour
+            period=3600,
+            timestamp=timestamp,
+            # full consumption
+            delta_min=1,
+            # depletes
+            delta_factor=-1,
+            # always running, determined by below
+            freeze=False,
+            extra_freeze_conditions=None,
+            # No real upper limit due to the possibility to stuff more 
+            # items into fuel bay because :ccp:
+            full=sys.maxint,
+            value=value,
+            # Empty level always 0.  Fortunately.
+            empty=0,
+        )
+
+    def isReinforced(self):
+        return self.tower.state == 'reinforced'
+
+
+def reinforceTower(tower):
+    tower.getResourceBay('Strontium')
--- mtj/eve/tracker/tests/test_basic.py
+++ mtj/eve/tracker/tests/test_basic.py
@@ -1,6 +1,24 @@
 from unittest import TestCase, TestSuite, makeSuite
 
-from mtj.multimer.buffer import Buffer, TimedBuffer
+from mtj.eve.tracker.pos import TowerResourceBuffer
+
+
+class TowerResourceBufferTestCase(TestCase):
+    """
+    Unit tests for the basic structures.
+    """
+
+    def setUp(self):
+        self.fuelbay = TowerResourceBuffer(None,
+            delta=40, timestamp=0, value=28000)
+
+    def test_0000_base(self):
+        self.assertEqual(self.fuelbay.value, 28000)
+        self.assertEqual(self.fuelbay.delta, 40)
+
+    def test_0001_nexthour(self):
+        fuelbay = self.fuelbay.getCurrent(timestamp=3600)
+        self.assertEqual(fuelbay.value, 27960)
 
 
 class StructureTestCase(TestCase):
@@ -20,5 +38,6 @@ class StructureTestCase(TestCase):
 
 def test_suite():
     suite = TestSuite()
+    suite.addTest(makeSuite(TowerResourceBufferTestCase))
     suite.addTest(makeSuite(StructureTestCase))
     return suite
