ba909f28bc1a125d6932239c9f31748e5bc82fd6
Tommy Yu <y@metatoaster.com>
2012-12-28 03:28:58 +1300

    Conforming the arguments to be similar to EVE API.
      - Also removed some poorly thought out classes.

--- mtj/eve/tracker/README.rst
+++ mtj/eve/tracker/README.rst
@@ -4,9 +4,67 @@ EVE POS Tracker
 The following is a brief introduction on the basic initialization and
 demonstration the pos tracker in action.
 
-First initialize a tower::
+Right now we don't have mocks, so set up a cache to limit hits::
+
+    >>> import tempfile
+    >>> import os.path
+    >>> from mtj.eve.tracker import cache
+    >>> cache_file = os.path.join(tempfile.gettempdir(), 'evelink_mjt.sqlite')
+    >>> cache.set_evelink_cache(cache_file)
+
+Tower initialization
+--------------------
+
+First initialize a tower.  This process will fetch the fuel requirements
+from evedb and the API for the empire currently controlling the system
+for the charter requirements::
 
     >>> from mtj.eve.tracker.pos import Tower
-    >>> tower = Tower('6VDT-H', 5, 2, 'online')
-    >>> sorted(tower.resources.keys())
-    ['Amarr Fuel Block', 'Charter', 'Strontium']
+    >>> tower1 = Tower(1000001, 12235, 30004608, 40291202, 4,
+    ...     1325376000, 1306886400, 498125261)
+    >>> tower1.initFuels()
+    >>> len(tower1.fuels)
+    2
+
+As the above is a null security system, no charters are required.  We
+can try again using one in high security space::
+
+    >>> from mtj.eve.tracker.pos import Tower
+    >>> tower2 = Tower(1000001, 12235, 30004268, 40270415, 4,
+    ...     1325376000, 1306886400, 498125261)
+    >>> tower2.initFuels()
+    >>> len(tower2.fuels)
+    3
+
+Adding/setting fuel levels
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The primary function of the pos tracker is to track fuel levels, and to
+do that the current fuel levels must be added.  The values are generally
+derived from the API, but they can be manually added.
+
+To set fuel levels, first verify that the levels needs updating::
+
+    >>> fuels = {
+    ...     4247: 12345,
+    ...     16275: 7200,
+    ... }
+    >>> sorted(tower1.verifyResources(fuels, 1338508800))
+    [4247, 16275]
+
+The return values are the fuel types that does not match the expected
+value given the input.  This means the fuel levels will need to be
+updated, like so::
+
+    >>> fuels = {
+    ...     4247: 12345,
+    ...     16275: 7200,
+    ... }
+    >>> sorted(tower1.verifyResources(fuels, 1338508800))
+    [4247, 16275]
+
+Day-to-day fuel calculation
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Back to the first tower.  As its owner's alliance has sovereignty in the
+system, the fuel consumption rate should reflect the discounts granted::
--- mtj/eve/tracker/pos.py
+++ mtj/eve/tracker/pos.py
@@ -1,8 +1,14 @@
 import sys
 
+from mtj.evedb.structure import ControlTower
+
 from mtj.multimer.buffer import TimedBuffer
 from mtj.multimer.timeline import Event
 from mtj.multimer.installation import Installation, InstallationFactory
+
+from mtj.eve.tracker.evelink import Helper
+
+pos_info = ControlTower()
 
 
 class SovChangeEvent(Event):
@@ -14,41 +20,7 @@ class SovChangeEvent(Event):
 class Reinforcement(Event):
     """
     Initiate strontium consumption and 
-    Double fuel consu
     """
-
-
-def lookupTower(itemid):
-    # XXX placeholder
-    items = {
-        12235: {
-            'name': 'Amarr Control Tower',
-            'fuelname': 'Amarr Fuel Block',
-            'fuelrate': 40,
-        }
-    }
-
-    return items.get(itemid, {})
-
-
-class TowerFactory(InstallationFactory):
-    """
-    Factory to build a POS.
-    """
-
-    def __init__(self):
-        pass
-    
-    def __call__(self, system, planet, moon, state, itemid):
-
-        info = lookupTower(itemid)
-
-        tower = Tower(system, planet, moon, state)
-        # register this into the database.
-
-        tower.itemid = itemid
-
-        return tower
 
 
 class Tower(Installation):
@@ -56,30 +28,102 @@ class Tower(Installation):
     A Player Owned Structure (POS).
     """
 
-    def __init__(self, system, planet, moon, state):
+    def __init__(self, itemID, typeID, locationID, moonID, state,
+            stateTimestamp, onlineTimestamp, standingOwnerID):
         """
         This should only be called once.
+
+        The parameters are equivalent to /corp/StarbaseList.xml.
         """
 
-        self.system = system
-        self.planet = planet
-        self.moon = moon
-        # one of {unanchored, anchoring, anchored, online, reinforced}
+        # Hurr why can't this be done automagically from arguments...
+        self.itemID = itemID
+        self.typeID = typeID
+        self.locationID = locationID
+        self.moonID = moonID
         self.state = state
+        self.onlineTimestamp = onlineTimestamp
+        self.standingOwnerID = standingOwnerID
 
-        # Can be updated, but generally derived from evedb
-        self.empire = None  # derived from system/evedb
-        self.itemid = None  # tower typeid
-        self.itemname = None  # derived, 'Amarr Control Tower'
-        self.fuelid = None  # derived, 4548 (?)
-        self.fuelname = None  # derived, 'Amarr Fuel Block'
-        self.fuelrate = None  # derived, 'Amarr Fuel Block'
+        # variable retrieved values
+        self.stateTimestamp = stateTimestamp
 
-        # demo
-        self.empire = False
-        self.itemname = 'Amarr Control Tower'
-        self.fuelname = 'Amarr Fuel Block'
-        self.base_fuel_rate = 40
+        # derived but fixed values
+        self.typeName = None
+        self.allianceID = None
+        self.celestialName = None
+        self.solarSystemName = None
+
+        # variable values
+        self.sov = None  # sovereignty discount
+
+        # The calculated values
+        self.fuels = {}  # fuel
+        self.resources = {}  # silos
+
+    def initFuels(self):
+        all_fuels = pos_info.getControlTowerResource(self.typeID)
+
+        helper = Helper()
+        sov_info = helper.sov[self.locationID]
+
+        # Determine fuel type from systemID + factionID
+        faction_id = sov_info['faction_id']
+
+        for fuel in all_fuels:
+            if fuel['factionID'] in (faction_id, None):
+                res_buffer = TowerResourceBuffer(self, 
+                    delta=fuel['quantity'],
+                    timestamp=0,  # defined later
+                    purpose=fuel['purpose'],
+                    value=0,  # defined later
+                    resourceTypeName=fuel['typeName'],
+                )
+                self.fuels[fuel['resourceTypeID']] = res_buffer
+
+    def updateFuels(self):
+        pass
+        # Determine if discount changed by
+        # - Get and store the allianceID (if applicable) XXX should be cached.
+        # - Get sovereignty data from api
+        # If none of these change, report fuel
+        # If changed, generate event so they will be saved, and update
+        # fuel rate calculations.
+
+    def verifyResources(self, values, timestamp):
+        """
+        Verify resources with the given values
+
+        values
+            The values of the fuel levels.  Generally acquired from the
+            API.
+        timestamp
+            The timestamp of the values.
+
+        returns a list of fuel_id that mismatch from the input value.
+        """
+
+        mismatches = []
+
+        for fuel_id, fuel_buffer in self.fuels.iteritems():
+            verifier = values.get(fuel_id)
+            if not verifier:
+                # Can't verify against unknown.
+                continue
+
+            calculated = fuel_buffer.getCurrent(timestamp)
+            if verifier != calculated:
+                mismatches.append(fuel_id)
+
+        return mismatches
+
+    def update(self):
+        if not self.fuel:
+            # fuel not initialized.  derive from database for current
+            # factionID and allianceID
+            self.init_fuels()
+
+        self.update_fuels()
 
         # delta is determined by the tower (or this parent), but still
         # have to be provided as a standard parameter (rather than a
@@ -88,24 +132,6 @@ class Tower(Installation):
         # mismatch, tower size mismatch, sov status mismatch there 
         # should be a way to override the delta until the condition
         # triggering this is corrected.
-
-        fuel_rate = self.fuel_rate
-
-        # The calculated moving parts
-        self.resources = {
-            self.fuelname: TowerResourceBuffer(
-                self, self.base_fuel_rate, None, 0),
-            'Strontium': TowerResourceBuffer(self, 1, None, 0, freeze=True),
-            'Charter': TowerResourceBuffer(self, 1, None, 0),
-        }
-
-        self.required = [self.fuelname]
-        if self.empire:
-            self.required.append('charter')
-
-    @property
-    def fuel_rate(self):
-        return self.base_fuel_rate
 
     def updateResources(self):
         raise NotImplementedError()
@@ -121,10 +147,11 @@ class TowerResourceBuffer(TimedBuffer):
     The base tower bay.
     """
 
-    def __init__(self, tower=None, delta=None, timestamp=None, value=0, 
-            *a, **kw):
+    def __init__(self, tower=None, delta=None, timestamp=None, purpose=None,
+            value=0, resourceTypeName=None, *a, **kw):
 
         self.tower = tower
+        self.resourceTypeName = resourceTypeName
 
         super(TowerResourceBuffer, self).__init__(
             delta=delta,
@@ -145,38 +172,6 @@ class TowerResourceBuffer(TimedBuffer):
             # Empty level always 0.  Fortunately.
             empty=0,
         )
-
-
-class TowerStrontiumBuffer(TowerResourceBuffer):
-
-    def __init__(self, tower=None, delta=None, timestamp=None, value=0, 
-            *a, **kw):
-
-        self.tower = tower
-
-        super(TowerResourceBuffer, self).__init__(
-            # Always unit size 1
-            delta=delta,
-            # one hour
-            period=3600,
-            timestamp=timestamp,
-            # full consumption
-            delta_min=1,
-            # depletes
-            delta_factor=-1,
-            # always running, determined by below
-            freeze=False,
-            extra_freeze_conditions=None,
-            # No real upper limit due to the possibility to stuff more 
-            # items into fuel bay because :ccp:
-            full=sys.maxint,
-            value=value,
-            # Empty level always 0.  Fortunately.
-            empty=0,
-        )
-
-    def isReinforced(self):
-        return self.tower.state == 'reinforced'
 
 
 def reinforceTower(tower):
