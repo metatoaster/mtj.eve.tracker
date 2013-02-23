c58354ac8776e310724921cf7c3b688590a9031a
Tommy Yu <y@metatoaster.com>
2013-02-23 17:12:17 +1300

    A test involving two towers.

--- mtj/eve/tracker/tests/test_sql.py
+++ mtj/eve/tracker/tests/test_sql.py
@@ -91,6 +91,46 @@ class SqlBackendTestCase(TestCase):
         # Fuel values assigned
         self.assertEqual(tower.getTimeRemaining(1326850000), 9200)
 
+    def test_2001_reinstantiate(self):
+        self.backend._conn.execute('insert into tower values '
+            '(1, 1000001, 12235, 30004608, 40291202, 4, 1325376000, '
+            '1306886400, 498125261)')
+        self.backend._conn.execute('insert into tower values '
+            '(2, 1000002, 20066, 30004268, 40270415, 4, 1325376000, '
+            '1306942573, 498125261)')
+
+        self.backend._conn.execute('insert into fuel values '
+            '(1, 1, 16275, 300, 1325366000, 7200)')
+        self.backend._conn.execute('insert into fuel values '
+            '(2, 1, 4247, 30, 1325366000, 22340)')
+
+        self.backend._conn.execute('insert into fuel values '
+            '(3, 2, 4246, 10, 1325376000, 360)')
+        self.backend._conn.execute('insert into fuel values '
+            '(4, 2, 16275, 100, 1325376000, 2200)')
+        self.backend._conn.execute('insert into fuel values '
+            '(5, 2, 24592, 1, 1325376000, 34)')
+
+        self.backend._conn.execute('insert into fuel values '
+            '(6, 1, 16275, 300, 1325376000, 7200)')
+        self.backend._conn.execute('insert into fuel values '
+            '(7, 1, 4247, 30, 1325376000, 12345)')
+
+        self.backend.reinstantiate()
+
+        # Fuel values correctly reassigned for both towers.
+        tower1 = self.backend.getTower(1)
+        self.assertEqual(tower1.getResources(1325376000), {
+            4247: 12345,
+            16275: 7200,
+        })
+        tower2 = self.backend.getTower(2)
+        self.assertEqual(tower2.getResources(1325376000), {
+            4246: 360,
+            16275: 2200,
+            24592: 34,
+        })
+
 def test_suite():
     suite = TestSuite()
     suite.addTest(makeSuite(SqlBackendTestCase))
