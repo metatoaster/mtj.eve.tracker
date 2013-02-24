a241c7f2c7f39d7002f132151aa1af224e1cb84c
Tommy Yu <y@metatoaster.com>
2013-02-24 22:30:46 +1300

    A demo manager that integrates API with tracker.
      - A simple test bed for accessing the API.

--- mtj/eve/tracker/manager.py
+++ mtj/eve/tracker/manager.py
@@ -1,7 +1,63 @@
-class TowerManager(object):
+import time
+import logging
+
+import zope.component
+
+from mtj.eve.tracker.interfaces import ITrackerBackend
+from mtj.eve.tracker import evelink
+
+logger = logging.getLogger('mtj.eve.pos.manager')
+
+
+class BaseTowerManager(object):
     """
-    The tower manager.
+    A class that gathers the loose bits of functions.
     """
 
-    def __init__(self, backend=None):
-        self.backend = backend
+    def __init__(self):
+        self.backend = zope.component.queryUtility(ITrackerBackend)
+        if not ITrackerBackend.providedBy(self.backend):
+            raise TypeError('No appropriate backend is registered')
+
+        # XXX set up evelink cache
+        self.logger = logger
+
+    def importWithApi(self, api):
+        """
+        Takes a fully prepared evelink API object (cache + keys) to
+        instantiate towers.
+        """
+
+        corp = evelink.Corp(api)
+        starbases = corp.starbases()
+        starbases_c = len(starbases)
+        logger.debug('%d starbases returned', starbases_c)
+
+        for c, item in enumerate(starbases.iteritems()):
+            k, v = item
+            logger.debug('(%d/%d) starbases processed.', c, starbases_c)
+            logger.debug('processing itemID: %d', k)
+            tower = self.backend.addTower(**v)
+
+            # Get time right before the request.
+            ts = time.time()
+            details = corp.starbase_details(k)
+
+            # XXX I have significant uncertainty on the timestamp, as it
+            # may be fast?
+
+            state = details['state']
+            if state == 'online':
+                # only online timestamp is sort of trustworthy, even
+                # though it might be some time in the future.
+                timestamp = details['state_ts']
+                delta = ts - timestamp
+                logger.debug('ts [%d] - state_ts [%d] = delta [%d]',
+                    ts, timestamp, delta)
+            else:
+                timestamp = ts
+                logger.debug('not online, using current time [%d]', ts)
+
+            tower.updateResources(details['fuel'], timestamp)
+
+        logger.debug('(%d/%d) processing complete', starbases_c, starbases_c)
