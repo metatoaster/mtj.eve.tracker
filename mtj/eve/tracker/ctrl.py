340c0fbcc7f968393ab59c63b506584390f3088e
Tommy Yu <y@metatoaster.com>
2013-07-27 22:41:58 +1200

    Core daemon mode
    - For the usual start/stop/restart/status commands.

--- mtj/eve/tracker/ctrl.py
+++ mtj/eve/tracker/ctrl.py
@@ -3,10 +3,23 @@ from __future__ import unicode_literals  # we are using json anyway.
 import argparse
 import cmd
 import code
+import errno
 import json
+import logging
 import os
+import signal
 import sys
 import tempfile
+
+try:
+    import daemon
+    from daemon.pidfile import TimeoutPIDLockFile
+    from daemon.runner import is_pidfile_stale
+    import lockfile
+    import pwd
+    HAS_DAEMON = True
+except ImportError:
+    HAS_DAEMON = False
 
 from mtj.eve.tracker.runner import BaseRunner, FlaskRunner
 
@@ -28,6 +41,11 @@ class Options(object):
         'api': {
             'source': 'config',
             'api_keys': {},
+        },
+        'daemon': {
+            'effective_user': None,
+            'pidfile': 'mtj_daemon.pid',
+            'working_directory': None,
         },
         'flask': {
             'host': '127.0.0.1',
@@ -55,6 +73,11 @@ class Options(object):
         'api': {
             'source': ('config', 'backend'),
             'api_keys': dict,
+        },
+        'daemon': {
+            'effective_user': basestring,
+            'pidfile': basestring,
+            'working_directory': basestring,
         },
         'flask': {
             'host': basestring,
@@ -123,29 +146,172 @@ class TrackerCmd(cmd.Cmd):
         self.app = app
         self.runner_factory = runner_factory
 
-    def run(self, config):
+    def make_runner(self, config):
         """
-        Run this thing.
+        Return an instantiated and configured runner instance.
         """
 
         runner = self.runner_factory()
         runner.configure(config=config)
+        return runner
+
+    def run(self, config):
+        """
+        Run the thing right away.
+        """
+
+        runner = self.make_runner(config)
         runner.initialize()
         runner.run(app=self.app)
+
+    def get_daemon_config(self):
+        return self.options.config.get('daemon')
+
+    def make_pid_file(self, timeout=0):
+        return TimeoutPIDLockFile(self.get_daemon_config().get('pidfile'),
+            timeout)
 
     def do_start(self, arg):
         """
         start the daemon (not implemented).
         """
 
-        print "daemon mode not implemented."
+        if not HAS_DAEMON:
+            print("The package python-daemon is not available, please use fg.")
+            return
+
+        # As the python-daemon implementation will close all file
+        # handlers by default, we have to defer the opening of all file
+        # handlers that may end up being referenced after the fork to
+        # prevent hilariously bad things from happening, like writing
+        # the logs into a database file.
+        logs = []
+
+        def get_uid_gid(username):
+            # Get the uid/gid from username to drop into post-startup
+            if not username:
+                return None, None
+            try:
+                pwrec = pwd.getpwnam(username)
+            except KeyError:
+                try:
+                    pwrec = pwd.getpwuid(int(username))
+                except (KeyError, ValueError):
+                    logs.append((logging.WARNING,
+                        'Invalid or unknown daemon.effective_user: `%r`',
+                        username))
+                    return None, None
+            if os.geteuid() == 0:
+                logs.append((logging.INFO,
+                    'Found daemon.effective_user: %s (uid=%d).',
+                    pwrec[0], pwrec[2]))
+                return pwrec[2], pwrec[3]
+            if os.geteuid() == pwrec[2]:
+                logs.append((logging.INFO,
+                    'Already running as daemon.effective_user: %s (uid=%d).',
+                    pwrec[0], pwrec[2]))
+                return None, None
+            logs.append((logging.WARNING,
+                'Process owner is not root; daemon.effective_user ignored.'))
+
+        def make_daemon_context():
+            config_daemon = self.get_daemon_config()
+            pidfile = self.make_pid_file()
+            uid, gid = get_uid_gid(config_daemon.get('effective_user'))
+            working_dir = config_daemon.get('working_directory')
+
+            if working_dir is None:
+                print('ERROR: working directory undefined; aborting.')
+                return
+
+            # need to figure out relative paths to pid?
+
+            return daemon.DaemonContext(**{
+                'pidfile': pidfile,
+                'uid': uid,
+                'gid': gid,
+                'stdin': sys.stdin,
+                'stdout': sys.stdout,
+                'stderr': sys.stderr,
+            })
+
+        dcontext = make_daemon_context()
+        if not dcontext:
+            print("ERROR: Fail to construct daemon context.")
+            return
+
+        pid = dcontext.pidfile.read_pid()
+        if pid:
+            if is_pidfile_stale(dcontext.pidfile):
+                print("Removing stale pid file.")
+                dcontext.pidfile.break_lock()
+            else:
+                print("Daemon already running (pid=%d)." % pid)
+                return
+
+        try:
+            print("Starting daemon; check logs for status.")
+            dcontext.open()
+        except (lockfile.LockTimeout, lockfile.AlreadyLocked):
+            print("ERROR: Failed to acquire pid lock file.")
+
+        # Elsie's homeworld opened.
+
+        logger = logging.getLogger('mtj.eve.tracker.ctrl')
+        runner = self.make_runner(self.options.config)
+        # Log the deferred logs.
+        for log in logs:
+            logger.log(*log)
+
+        logger.info("Daemon running (pid=%d).", dcontext.pidfile.read_pid())
+
+        try:
+            runner.initialize()
+            runner.run(app=self.app)
+        except SystemExit as e:
+            logger.info('%s', e)
+        except:
+            logger.exception("Unexpected error broke the runner.")
 
     def do_stop(self, arg):
         """
         stop the daemon (not implemented).
         """
 
-        print "can't stop what's not implemented"
+        pidfile = self.make_pid_file()
+        pid = pidfile.read_pid()
+        if not pid:
+            print("Daemon not running.")
+            return
+        if is_pidfile_stale(pidfile):
+            print("Removing stale pid file.")
+            pidfile.break_lock()
+            return
+
+        try:
+            os.kill(pid, signal.SIGTERM)
+            try:
+                pidfile.acquire(5)
+            except lockfile.LockTimeout:
+                print("Daemon still running (pid=%d)." % pid)
+            else:
+                pidfile.release()
+                print("Daemon process terminated.")
+        except OSError as e:
+            print("Error terminating daemon (pid=%d)." % pid)
+
+    def do_restart(self, arg):
+        self.do_stop(arg)
+        self.do_start(arg)
+
+    def do_status(self, arg):
+        pidfile = self.make_pid_file()
+        pid = pidfile.read_pid()
+        if not pid:
+            print("Daemon not running.")
+            return
+        if not is_pidfile_stale(pidfile):
+            print("Daemon running (pid=%d)." % pid)
 
     def do_fg(self, arg):
         """
@@ -166,7 +332,7 @@ class TrackerCmd(cmd.Cmd):
         options.update(self.options.config)
 
         if arg:
-            print 'Notice: `%s` will be notified of update.' % arg
+            print('Notice: `%s` will be notified of update.' % arg)
 
         runner = self.runner_factory()
         runner.configure(config=options.config)
@@ -180,8 +346,8 @@ class TrackerCmd(cmd.Cmd):
 
         if arg:
             p = options.config['mtj.eve.tracker.runner.FlaskRunner']
-            print requests.post(arg, data='{"key": "%(admin_key)s"}' % p
-                ).content
+            print(requests.post(arg, data='{"key": "%(admin_key)s"}' % p
+                ).content)
 
     def do_debug(self, arg):
         """
@@ -222,7 +388,7 @@ class TrackerCmd(cmd.Cmd):
         """
 
         if not arg:
-            print "usage: read_config <filename>"
+            print("usage: read_config <filename>")
             return
 
         try:
@@ -230,12 +396,12 @@ class TrackerCmd(cmd.Cmd):
             try:
                 self.options.load_config(fd)
             except TypeError as e:
-                print 'invalid config file `%s`, %s' % (arg, str(e))
+                print('invalid config file `%s`, %s' % (arg, str(e)))
             except ValueError as e:
-                print 'invalid config file `%s`, %s' % (arg, str(e))
+                print('invalid config file `%s`, %s' % (arg, str(e)))
             fd.close()
         except IOError:
-            print 'cannot load config from file `%s`' % arg
+            print('cannot load config from file `%s`' % arg)
 
     def do_write_config(self, arg):
         """write the config file.
@@ -244,16 +410,18 @@ class TrackerCmd(cmd.Cmd):
         """
 
         if not arg:
-            print "usage: write_config <filename>"
+            print("usage: write_config <filename>")
             return
 
         if os.path.exists(arg):
-            print '`%s` already exists' % arg
+            print('`%s` already exists' % arg)
             return
 
         try:
             td = tempfile.TemporaryFile()
             self.options.dump_config(td)
+            # TODO maybe set the daemon working dir to the target dir if
+            # it's undefined.
             td.seek(0)
             try:
                 fd = open(arg, 'w')
@@ -262,12 +430,12 @@ class TrackerCmd(cmd.Cmd):
             finally:
                 td.close()
         except IOError:
-            print 'cannot write config to file `%s`' % arg
+            print('cannot write config to file `%s`' % arg)
         except TypeError as e:
-            print 'configuration cannot be written, %s' % str(e)
+            print('configuration cannot be written, %s' % str(e))
 
     def do_EOF(self, arg):
-        print
+        print('')
         return 1
 
 
@@ -280,6 +448,7 @@ def get_argparsers():
     sp_start = sp.add_parser(r'start', help='Starts %(prog)s daemon')
     sp_stop = sp.add_parser(r'stop', help='Stops %(prog)s daemon')
     sp_restart = sp.add_parser(r'restart', help='Restarts %(prog)s daemon')
+    sp_status = sp.add_parser(r'status', help='Get daemon status')
     sp_fg = sp.add_parser(r'fg', help='Run %(prog)s in foreground')
     sp_import = sp.add_parser(r'import', help='Imports API data')
     sp_debug = sp.add_parser(r'debug', help='Open a debug python shell')
